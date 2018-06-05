f207de1e1e1d84d6a14a5d255732959fe07aa09e
Harry Hodgson <harry.hodgson@dubitlimited.com>
2018-06-05 14:54:50 +0100

    Added internal utils call for create, loading and instantiating

---
+++ Utils.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace DUCK.HieriarchyBehaviour
+{
+	internal static class Utils
+	{
+		internal static TBehaviour CloneBehaviour<TBehaviour>(TBehaviour behaviourToClone, Transform parent)
+			where TBehaviour : MonoBehaviour
+		{
+			var behaviour = Object.Instantiate(behaviourToClone, parent);
+			behaviour.name = behaviourToClone.name;
+			behaviour.transform.localPosition = Vector3.zero;
+			return behaviour;
+		}
+
+		internal static TBehaviour CreateGameObjectWithBehaviour<TBehaviour>(Transform parent)
+			where TBehaviour : MonoBehaviour
+		{
+			var behaviour = new GameObject(typeof(TBehaviour).Name).AddComponent<TBehaviour>();
+			behaviour.transform.SetParent(parent.transform);
+			behaviour.transform.localPosition = Vector3.zero;
+			return behaviour;
+		}
+
+		internal static TBehaviour InstantiateResource<TBehaviour>(string path, Transform parent, bool worldPositionStay = true)
+			where TBehaviour : MonoBehaviour
+		{
+			var loadedBehaviour = Resources.Load<TBehaviour>(path);
+			var behaviour = Object.Instantiate(loadedBehaviour, parent, worldPositionStay);
+			behaviour.name = loadedBehaviour.name;
+			return behaviour;
+		}
+	}
+}
