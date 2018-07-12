c4acd6e3b4fec5c696fbafe294eacb02788a1873
Harry Hodgson <harry.hodgson@dubitlimited.com>
2018-07-12 17:21:00 +0100

    Updated tests to work with 2018.2

---
+++ TestBehaviours/HierarchyBehaviourWithArgs.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace DUCK.HieriarchyBehaviour.TestBehaviours
+{
+	[AddComponentMenu("")]
+	public class HierarchyBehaviourWithArgs : MonoBehaviour, IHierarchyBehaviour<string>
+	{
+		public string Args { get; private set; }
+		public bool DidInitialize { get; private set; }
+
+		public void Initialize(string args)
+		{
+			DidInitialize = true;
+			Args = args;
+		}
+	}
+}
